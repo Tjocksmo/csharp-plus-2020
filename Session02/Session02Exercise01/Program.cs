9bba491b6b18283ad1952a23ab88c3739f95d955
PROGRAMMERINGEN\Patrik <patrikpersson45@gmail.com>
2020-08-26 10:40:11 +0200

    Innehåller sträng

--- Session02/Session02Exercise01/Program.cs
+++ Session02/Session02Exercise01/Program.cs
@@ -7,6 +7,10 @@ namespace Session02Exercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string stringValue = "My string is cool!";
+
+            Console.WriteLine($"The string value of stringValie is {stringValue}");
         }
     }
 }
